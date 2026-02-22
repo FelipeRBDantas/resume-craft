8cd5feebba779de26e9744b5a52b178a11ceeeab
feliperbdantas <feliperbdantas@outlook.com>
2026-02-22 20:15:01 -0300

    feat: resume page - creating the reusable page and modal

--- src/components/pages/dashboard/resumes/resumes-list/index.tsx
+++ src/components/pages/dashboard/resumes/resumes-list/index.tsx
@@ -1,9 +1,10 @@
+import { AddResumeButton } from "../add-resume-button";
 import { ResumeCard } from "../resume-card";
 
 export const ResumeList = () => {
   return (
     <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 auto-rows-max gap-4 flex-1">
-      <ResumeCard />
+      <AddResumeButton />
 
       <ResumeCard />
 
