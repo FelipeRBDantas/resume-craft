e82a4ab3480126dfec87c9db5c1e39f1f0de4fdd
feliperbdantas <feliperbdantas@outlook.com>
2026-03-01 17:29:37 -0300

    feat: cv page: data - structuring resizable panels

---
+++ src/components/pages/dashboard/resume/resume-content/index.tsx
@@ -0,0 +1,5 @@
+export const ResumeContent = () => {
+  return (
+    <section className="w-full h-full overflow-hidden flex items-center justify-center relative bg-muted dark:background"></section>
+  );
+};
