147c7f3b6e2ce8ab3bd12d0feee6260ba1927b94
feliperbdantas <feliperbdantas@outlook.com>
2026-03-01 18:14:31 -0300

    feat: cv page: data - basic information

--- src/components/pages/dashboard/resume/infos-sidebar/index.tsx
+++ src/components/pages/dashboard/resume/infos-sidebar/index.tsx
@@ -1,3 +1,14 @@
+import Link from "next/link";
+import Logo from "@/assets/logo.svg";
+
 export const InfosSidebar = () => {
-  return <aside className="w-full h-full p-6 overflow-y-auto"></aside>;
+  return (
+    <aside className="w-full h-full p-6 overflow-y-auto">
+      <div className="w-full flex items-center justify-between">
+        <Link href="/dashboard/resumes">
+          <Logo className="w-full max-w-[80px]" />
+        </Link>
+      </div>
+    </aside>
+  );
 };
