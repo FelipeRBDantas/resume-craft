1b07ad5d953faf630b03179fb73b85af433439d0
feliperbdantas <feliperbdantas@outlook.com>
2026-04-02 15:12:07 -0300

    feat: resume page: structure - section: languages

--- src/components/pages/dashboard/resume/structure-sidebar/sections/language.tsx
+++ src/components/pages/dashboard/resume/structure-sidebar/sections/language.tsx
@@ -1,6 +1,45 @@
 import { Languages } from "lucide-react";
 import { SectionTitle } from "../../infos-sidebar/section-title";
 import { Controller, useFormContext } from "react-hook-form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+type LanguageOption = {
+  label: string;
+  value: ResumeLanguages;
+};
+
+export const languagesOptions: LanguageOption[] = [
+  {
+    label: "Inglês",
+    value: "english",
+  },
+  {
+    label: "Espanhol",
+    value: "spanish",
+  },
+  {
+    label: "Francês",
+    value: "french",
+  },
+  {
+    label: "Alemão",
+    value: "german",
+  },
+  {
+    label: "Italiano",
+    value: "italian",
+  },
+  {
+    label: "Português",
+    value: "portuguese",
+  },
+];
 
 export const LanguageSection = () => {
   const { control } = useFormContext<ResumeData>();
@@ -12,7 +51,21 @@ export const LanguageSection = () => {
       <Controller
         control={control}
         name="structure.language"
-        render={({ field }) => <></>}
+        render={({ field }) => (
+          <Select value={field.value} onValueChange={field.onChange}>
+            <SelectTrigger className="mt-4">
+              <SelectValue placeholder="Selecione uma linguagem" />
+            </SelectTrigger>
+
+            <SelectContent>
+              {languagesOptions.map((option) => (
+                <SelectItem key={option.value} value={option.value}>
+                  {option.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        )}
       />
     </div>
   );
