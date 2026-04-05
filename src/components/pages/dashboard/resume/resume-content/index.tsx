4a346d550b6b88055f91b2c0afccc470aa19ae52
feliperbdantas <feliperbdantas@outlook.com>
2026-04-05 20:05:56 -0300

    feat: resume page: structure - section: content

--- src/components/pages/dashboard/resume/resume-content/index.tsx
+++ src/components/pages/dashboard/resume/resume-content/index.tsx
@@ -4,12 +4,17 @@ import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import { ResumeTemplate } from "./templates";
 import { TransformControls } from "./controls";
 import { NavigationHeader } from "./header";
+import { useFormContext } from "react-hook-form";
 
 export const ResumeContent = () => {
+  const { watch } = useFormContext<ResumeData>();
+
+  const data = watch();
+
   return (
     <section className="w-full h-full overflow-hidden flex items-center justify-center relative bg-muted dark:background">
       <TransformWrapper
-        initialScale={0.5}
+        initialScale={0.45}
         minScale={0.4}
         centerOnInit
         centerZoomedOut
@@ -21,7 +26,7 @@ export const ResumeContent = () => {
           <TransformControls />
 
           <TransformComponent>
-            <ResumeTemplate />
+            <ResumeTemplate data={data} />
           </TransformComponent>
         </>
       </TransformWrapper>
