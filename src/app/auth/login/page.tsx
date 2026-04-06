2e605f8582dcd0b136d1f788c2c8f032de0b85c2
feliperbdantas <feliperbdantas@outlook.com>
2026-04-06 13:13:13 -0300

    feat: authentication

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,4 +1,8 @@
 import Image from "next/image";
+import Logo from "@/assets/logo.svg";
+import { ThemeToggle } from "@/components/shared/theme-toggle";
+import { Button } from "@/components/ui/button";
+import { Chrome, Github } from "lucide-react";
 
 export default function LoginPage() {
   return (
@@ -14,7 +18,40 @@ export default function LoginPage() {
         />
       </aside>
 
-      <form></form>
+      <form className="p-10 flex justify-center flex-col">
+        <div className="flex items-center justify-between mb-10">
+          <Logo className="max-w-[90px]" />
+
+          <ThemeToggle />
+        </div>
+
+        <h1 className="text-2xl font-title font-bold">Entre em sua conta</h1>
+
+        <p className="text-sm text-muted-foreground">
+          Caso não tenha conta, ela será criada automaticamente.
+        </p>
+
+        <div className="flex flex-col gap-4 mt-6">
+          <Button
+            variant="outline"
+            className="w-full gap-2"
+            type="button"
+            name="provider"
+            value="github"
+          >
+            <Github size={20} /> Entrar com GitHub
+          </Button>
+
+          <Button
+            className="w-full gap-2"
+            type="button"
+            name="provider"
+            value="google"
+          >
+            <Chrome size={20} /> Entrar com Google
+          </Button>
+        </div>
+      </form>
     </div>
   );
 }
